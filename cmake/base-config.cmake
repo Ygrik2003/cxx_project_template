974ab72f50adbf7fb687f58e34f4bcb795e5ae18
Stepanov Igor <i.stepanov@rit.va>
2026-07-28 09:41:26 +0300

    add android (cant work, ndk doesnt support import std)

--- cmake/base-config.cmake
+++ cmake/base-config.cmake
@@ -1,17 +1,11 @@
-set(CMAKE_CXX_MODULE_STD ON)
-
 add_library(template_coverage INTERFACE)
 target_compile_options(
     template_coverage
-    INTERFACE
-        -fprofile-instr-generate
-        -fcoverage-mapping
+    INTERFACE -fprofile-instr-generate -fcoverage-mapping
 )
 target_link_options(
     template_coverage
-    INTERFACE
-        -fprofile-instr-generate
-        -fcoverage-mapping
+    INTERFACE -fprofile-instr-generate -fcoverage-mapping
 )
 add_library(template::coverage ALIAS template_coverage)
 
@@ -22,16 +16,11 @@ target_compile_definitions(
 )
 target_compile_options(
     template_base
-    INTERFACE
-        -Werror
-        -Wpedantic
-        -Wall
-        $<$<CXX_COMPILER_ID:GNU>:-freflection>
+    INTERFACE -Werror -Wpedantic -Wall $<$<CXX_COMPILER_ID:GNU>:-freflection>
 )
 target_link_libraries(
     template_base
-    INTERFACE
-        $<$<BOOL:$ENV{TLT_COVERAGE}>:template::coverage>
+    INTERFACE $<$<BOOL:$ENV{TLT_COVERAGE}>:template::coverage>
 )
 
 add_library(template::base ALIAS template_base)
---
+++ cmake/toolchains/clang-android.cmake
@@ -0,0 +1,4 @@
+include($ENV{TLT_ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
+
+set(CMAKE_ANDROID_PLATFORM 24)
